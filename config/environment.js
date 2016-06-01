0f959aafdaec5807b91c02278715fbbf49b15f24
BrianSipple <Bsipple57@gmail.com>
2016-05-31 20:52:35 -0700

    update baseURL

--- config/environment.js
+++ config/environment.js
@@ -11,7 +11,7 @@ module.exports = function(environment) {
   const ENV = {
     modulePrefix: 'droplet',
     environment: environment,
-    baseURL: '/droplet/',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
